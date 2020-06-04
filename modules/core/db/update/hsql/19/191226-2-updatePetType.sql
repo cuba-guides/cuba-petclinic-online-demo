242ff6a8e83961a2532cc67aca946886f194a9c2
Mario <mario@die-davids.de>
2020-06-04 09:54:59 +0200

    update to latest upstream master; deleted not-relevant update scripts

--- modules/core/db/update/hsql/19/191226-2-updatePetType.sql
+++
@@ -1,3 +0,0 @@
-alter table PETCLINIC_PET_TYPE add column COLOR varchar(6) ^
-update PETCLINIC_PET_TYPE set COLOR = 'ffffff' where COLOR is null ;
-alter table PETCLINIC_PET_TYPE alter column COLOR set not null ;
