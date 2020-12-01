738dbcc3d9bc8a22219ca513a8438ea58120e05f
Natalya Firstova <firstova@haulmont.com>
2020-12-01 16:02:00 +0400

    Upgrade platform version to 7.2.10, helium addon version to 0.3.1, restapi addon version to 7.2.2

--- modules/web/test/com/haulmont/sample/petclinic/web/PetclinicWebTestContainer.java
+++ modules/web/test/com/haulmont/sample/petclinic/web/PetclinicWebTestContainer.java
@@ -8,10 +8,10 @@ import java.util.Arrays;
 public class PetclinicWebTestContainer extends TestContainer {
 
     public PetclinicWebTestContainer() {
-        appComponents = new ArrayList<>(Arrays.asList(
-                "com.haulmont.cuba"
-                // add CUBA add-ons and custom app components here
-        ));
+        appComponents = Arrays.asList(
+                "com.haulmont.cuba",
+                "com.haulmont.addon.restapi",
+                "com.haulmont.addon.helium");
         appPropertiesFiles = Arrays.asList(
                 // List the files defined in your web.xml
                 // in appPropertiesConfig context parameter of the web module
